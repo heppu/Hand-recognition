4fa185e2de4dae6c956f3efc527d6ae65ef69ead
Henri Koski <henri.t.koski@gmail.com>
2014-12-07 22:11:45 +0200

    multiple changes

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 Hand-recognition
 ================
 
-Hand recognition is a python script that detects hand from webcam and calculates it coordinates and area.
+Hand recognition is a python script that detects hand from image and calculates its coordinates and area.
 To use script you need to install [SimpleCV](https://github.com/sightmachine/simplecv).
+
+
+Settings
+--------
+WEBCAM = True
+If WEBCAM is set to True script will read images from webcam strem.
+
+URL = "http://localhost:2700/drone"
+If WEBCAM is set to False and URL is defined script will get images from url.
+
+WATCH = True
+If WATCH is set to True you will see image and blue circle around hand if one is found.
--- hand_recognition.py
+++ hand_recognition.py
@@ -1,58 +1,145 @@
+import sys
 import SimpleCV
-import math
 import datetime
- 
-try:
-	cam = SimpleCV.Camera()
-except:
-	print "Can't open webcam"
+import urllib2
+import StringIO
+from PIL import Image
+
+# Change WEBCAM to True if you want to read images from webcam
+WEBCAM = True
+
+# If webcam is False define URL where images are fetched
+URL = ""
+
+# Change to True if you want to see images
+WATCH = True
+
+if(WEBCAM==False and len(URL)==0):
+	print "Use webcam or define url"
 	sys.exit()
 
-display = SimpleCV.Display()
-normaldisplay = True
-
+# Load HaarCascade files
 hand_xml_a = SimpleCV.HaarCascade("xml/handa.xml") 	# hand fingers up
 hand_xml_b = SimpleCV.HaarCascade("xml/handb.xml") 	# hand fingers left to right
 hand_xml_c = SimpleCV.HaarCascade("xml/hands.xml") 	# hand fingers left to right
 
+i=0
 
-while display.isNotDone():
- 	start = datetime.datetime.now()
-	if display.mouseRight:
-		normaldisplay = not(normaldisplay)
-	
-	img = cam.getImage().flipHorizontal()
+def analyze(img, i):
+	start = datetime.datetime.now()
+	# Mask skintone from image
 	masked = img.getSkintoneMask()
 
-	hands = masked.findHaarFeatures(hand_xml_d)
+	# Start from previous angle for faster recognition
+	if(i!=0):
+		masked = masked.rotate(i*90, fixed=False)
 
-	circe_layer = SimpleCV.DrawingLayer((img.width, img.height))
+	# Check for hand in every direction
+	for j in xrange(0, 4):
+		hands = masked.findHaarFeatures(hand_xml_b)
+		if hands:
+			break
+		else:
+			masked = masked.rotate(90, fixed=False)
+			i = i+1
+			if(i==4):
+				i=0
+	
 	percent = 0
 	x = 0
 	y = 0
 
-	print hands
-
 	if hands:
-		x, y = hands[0].coordinates()
+		# Coordinate transform
+		x_in, y_in = hands[0].coordinates()
+		if(i==0):
+			x = x_in
+			y = y_in
+		elif(i==1):
+			x = img.width - y_in
+			y = x_in
+		elif(i==2):
+			x = img.width - x_in
+			y = img.height - y_in
+		elif(i==3):
+			x = y_in	
+			y = img.height - x_in
+
+		# Movement calculation
 		x_out = (x - (img.width/2))/float(img.width/2) * 100 				# x coordinate between -100 and 100
 		y_out = (-(y - (img.height/2)))/float(img.height/2) * 100			# y coordinate between -100 and 100
 		radius = (hands[0].boundingBox()[2] + hands[0].boundingBox()[3])/4	# radius for circl
 		percent = hands[0].area() / float(img.width*img.height) * 100 		# area that hand covers between 0 and 100
 
-		circe_layer.circle(hands[0].coordinates(), radius, color=SimpleCV.Color.BLUE, width=5)
-		
-		masked.addDrawingLayer(circe_layer)
- 		masked.applyLayers()
- 		img.addDrawingLayer(circe_layer)
- 		img.applyLayers()	
+		# Image Drawing
+		if(WATCH):
+			circe_layer = SimpleCV.DrawingLayer((img.width, img.height))
+			circe_layer.circle((x,y), radius, color=SimpleCV.Color.BLUE, width=5)
+			masked.addDrawingLayer(circe_layer)
+	 		masked.applyLayers()
+	 		img.addDrawingLayer(circe_layer)
+	 		img.applyLayers()
 
-		#print x_out, y_out, percent	
+		print x_out, y_out, percent	
 
  	time =  (datetime.datetime.now() - start).total_seconds() * 1000
  	print "Image analyzed in %sms" % (time)
-	
-	if normaldisplay:
-		img.show()
+
+ 	# Display images
+ 	if(WATCH):
+		if normaldisplay:
+			img.show()
+		else:
+			masked.rotate(360-(i*90), fixed=False).show()
+
+	return i
+
+
+if(WEBCAM):
+	#Open webcam
+	try:
+		cam = SimpleCV.Camera()
+	except:
+		print "Can't open webcam"
+		sys.exit()
+
+	if(WATCH):	
+		display = SimpleCV.Display()
+		normaldisplay = True
+		while display.isNotDone():
+			if display.mouseRight:
+				normaldisplay = not(normaldisplay)
+			# Read image from webcam
+			img = cam.getImage().flipHorizontal()
+			#analyze image
+			i = analyze(img, i)
 	else:
-		masked.show()
+		while True:
+			# Read image from webcam
+			img = cam.getImage().flipHorizontal()
+			#analyze image
+			i = analyze(img, i)
+
+
+else:	
+	if(WATCH):	
+		display = SimpleCV.Display()
+		normaldisplay = True
+		while display.isNotDone():
+			if display.mouseRight:
+				normaldisplay = not(normaldisplay)
+			# Read imger from url
+			im = urllib2.urlopen(URL).read()
+			img = Image.open(StringIO.StringIO(im))
+			img = SimpleCV.Image(img)
+			#analyze image
+			i = analyze(img, i)
+
+	else:
+		while True:
+			# Read imger from url
+			im = urllib2.urlopen(URL).read()
+			img = Image.open(StringIO.StringIO(im))
+			img = SimpleCV.Image(img)
+			#analyze image
+			i = analyze(img, i)
